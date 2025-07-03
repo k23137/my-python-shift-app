f56c032b00f15a31b354031d6ac7ac98ec079df4
k23137 <k23137kk@aitech.ac.jp>
2025-07-04 01:57:26 +0900

    Supabaseへのデータベース移行

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     # ローカル開発用DB_URL（RenderのPostgreSQL接続文字列をここに貼り付けるか、別途ローカルDBを用意）
     # 例: postgresql://admin:password@localhost:5432/shift_db
     # Renderにデプロイする際は、Render側で設定するDATABASE_URL環境変数が優先されます。
-    DATABASE_URL = os.environ.get('DATABASE_URL') or "postgresql://postgres:kF3JMmgHNRy2X32@db.ylhqufcfdzbofzxspjky.supabase.co:5432/postgres"
+    DATABASE_URL = os.environ.get('DATABASE_URL') or "postgresql://postgres.ylhqufcfdzbofzxspjky:kF3JMmgHNRy2X32@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres"
     
 
     # 上の行の <your_render_user> ... の部分をあなたの実際の接続文字列で置き換えてください
